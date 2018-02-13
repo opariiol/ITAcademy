fbd20bfd7efbd17b456eb351f9adf1929c8b6b33
NikitaChelombitko <timdeygun@yandex.ru>
2018-02-13 18:43:21 +0200

    Fixed access modificators.

--- src/ua/org/oa/evlashdv/Hello.java
+++
@@ -1,7 +0,0 @@
-package ua.org.oa.evlashdv;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
--- src/ua/org/oa/evlashdv/lecture5/App.java
+++
@@ -1,7 +0,0 @@
-package ua.org.oa.evlashdv.lecture5;
-
-public class App {
-    public static void main(String[] args) {
-
-    }
-}
