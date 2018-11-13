41aeb1f3cee7f8b60f52a2375b0788e53cb53d26
opariiol <olga.oparii@gmail.com>
2018-11-13 13:10:36 +0200

    Done first part of task 1

---
+++ ItAcademy/src/myProjects/Task_1/Part_1_1/AppForSimpleThread.java
@@ -0,0 +1,61 @@
+package myProjects.Task_1.Part_1_1;
+
+import myProjects.Task_1.Part_1_1.DataForSimpleNumbers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class AppForSimpleThread {
+
+    public static void main(String[] args) {
+        // input data
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter first number of interval: ");
+        int firstNumber = scan.nextInt(); // the first element of the list of numbers
+        System.out.println("Enter last number of interval: ");
+        int lastNumber = scan.nextInt(); // the last element of the list of numbers
+        System.out.println("Enter count of threads");
+        int countThreads = scan.nextInt(); // we decide, how many threads we will start in this case
+        int lastNumberInInterval = lastNumber/countThreads; // this will be last number in every thread
+
+        // array with simple number for every thread
+        DataForSimpleNumbers counter [] = new DataForSimpleNumbers[countThreads];
+        // list for data save from our threads
+        List[] simpleNumbers = new ArrayList[countThreads];
+
+        // distribution of data across all threads
+        for (int i = 1; i < countThreads; i ++){
+            List<Integer> listForExactThread = new ArrayList<>();
+            counter[i] = new DataForSimpleNumbers(lastNumberInInterval*i,lastNumberInInterval*i +
+                    lastNumberInInterval, listForExactThread);
+            counter[i].setName("Counter № "+i);
+            simpleNumbers[i] = listForExactThread;
+        }
+
+        // divide the list of numbers block into intervals and run in threads
+        for (int i = 0; i < countThreads; i ++){
+            List<Integer> listForExactThread = new ArrayList<>();
+            counter[0] = new DataForSimpleNumbers(firstNumber, lastNumber,listForExactThread);
+            counter[i].start();
+            counter[0].run();
+            simpleNumbers[0] = listForExactThread;
+        }
+
+        // if our thread finds exception and blocks, we will check it with block try-catch
+        try {
+            for (int i = 0 ; i < countThreads; i ++){
+                counter[i].join();
+            }
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        System.out.println( "Our List of data:" + Arrays.toString(simpleNumbers)); // and there I don't understand,
+                                                                                    // why I have not one array
+
+    }
+
+
+}
---
+++ ItAcademy/src/myProjects/Task_1/Part_1_1/DataForSimpleNumbers.java
@@ -0,0 +1,41 @@
+package myProjects.Task_1.Part_1_1;
+
+import java.sql.SQLOutput;
+import java.util.List;
+
+public class DataForSimpleNumbers extends Thread{
+
+    private int firstNum;
+    private int lastNum;
+    private List<Integer> simpleNumberList;
+
+    public DataForSimpleNumbers(int firstNum, int lastNum, List<Integer> simpleNumberList) {
+        this.firstNum = firstNum;
+        this.lastNum = lastNum;
+        this.simpleNumberList = simpleNumberList;
+    }
+
+    @Override
+    public void run (){
+        while (firstNum <= lastNum){
+            boolean isPrime = checkSimpleNumber(firstNum);
+            if (isPrime){
+                simpleNumberList.add(firstNum);
+            }
+            firstNum += 1;
+        }
+        System.out.println("Counter № " + getName() + " has finisher work in this thread. ");
+
+    }
+
+    public static boolean checkSimpleNumber (int data){ // we check does it simple number
+        for (int i = 2; i <= data/2; i++) {
+            if((data % i == 0) & (i % 2 == 0)) return false;
+        }
+        return true;
+    }
+
+
+
+}
+
